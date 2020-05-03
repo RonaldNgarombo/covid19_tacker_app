156fa6277e9182a7389826ad64adfb02760230a0
The Code Artisan <ronaldngzgadro@gmail.com>
2020-05-03 17:27:06 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// Screens
+import HomeStackScreen from './screens/HomeStackScreen';
+import AboutScreen from './screens/AboutScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ focused, color, size }) => {
+						let iconName;
+
+						if (route.name === 'Home') {
+							iconName = focused ? 'md-home' : 'md-home';
+						} else if (route.name === 'About') {
+							iconName = focused
+								? 'md-information-circle-outline'
+								: 'md-information-circle-outline';
+						}
+
+						// You can return any component that you like here!
+						return (
+							<Ionicons
+								name={iconName}
+								size={size}
+								color={color}
+							/>
+						);
+					},
+				})}
+				tabBarOptions={{
+					activeTintColor: '#ffffff',
+					inactiveTintColor: 'gray',
+					style: {
+						backgroundColor: '#2957AD',
+					},
+				}}
+			>
+				<Tab.Screen name="Home" component={HomeStackScreen} />
+				<Tab.Screen name="About" component={AboutScreen} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Tab = createBottomTabNavigator();
